6ba7d8e24333f10ce6d104692b6b565cbf4ba055
Bernardo Souza <bernardo@handcom.com.br>
2019-04-29 14:39:22 -0300

    Criada view de detalhe do item.

--- Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -168,5 +168,24 @@ namespace Spice.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        //GET Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MenuItemVM.MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
+            MenuItemVM.SubCategory = await _db.SubCategory.Where(s => s.CategoryId == MenuItemVM.MenuItem.CategoryId).ToListAsync();
+
+            if (MenuItemVM.MenuItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(MenuItemVM);
+        }
     }
 }
