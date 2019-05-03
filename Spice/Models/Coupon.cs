ba5f3e4947c076e24d2ef967c425ed6f267752a3
Bernardo Souza <bernardo@handcom.com.br>
2019-05-03 14:53:49 -0300

    Coupon controller actions de get e post, para index e view de edit e detalhe

--- Spice/Models/Coupon.cs
+++ Spice/Models/Coupon.cs
@@ -11,22 +11,29 @@ namespace Spice.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Coupon")]
         [Required]
         public string Name { get; set; }
 
+        [Display(Name = "Tipo de Coupon")]
         [Required]
         public string CouponType { get; set; }
 
         public enum ECouponType { Porcentagem=0, Reais=1 }
 
+        [Display(Name = "Total de desconto")]
         [Required]
         public double Discount { get; set; }
 
+        [Display(Name = "Valor Minimo")]
         [Required]
         public double MinimumAmount { get; set; }
 
+        [Display(Name = "Imagem")]
+        [Required]
         public byte[] Picture { get; set; }
 
+        [Display(Name = "Ativo")]
         public bool isActive { get; set; }
     }
 }
